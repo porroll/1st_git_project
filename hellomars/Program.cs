81f2dda3f0a5dc5aad6489e92037a4af8c773a78
Paul Stewart <pstewart@ukcloud.com>
2023-01-13 16:47:44 +0000

    creates new .Net 7.0 application called hellomars

---
+++ hellomars/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+[DebuggerDisplay($"{{{nameof(GetDebuggerDisplay)}(),nq}}")]
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        string myName = args[0];
+        Console.WriteLine("Hello, MARS! from " + myName);
+    }
+
+    private string GetDebuggerDisplay()
+    {
+        return ToString();
+    }
+}
